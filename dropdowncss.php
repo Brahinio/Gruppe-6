5cceb98fb82c38a363efbd6fac48330a08a5f67a
Mathias Tellefsen <mathias-ds@hotmail.com>
2017-03-26 13:41:51 +0200

    Changed meny size, decreased animation time

--- dropdowncss.php
+++ dropdowncss.php
@@ -5,15 +5,15 @@
             left: 0;
             top: 100px;
             width: 1000px;
-            height: 300px;
+            height: 100px;
             background-color: #87CEFA;
             margin-right: auto; 
             margin-left: auto;
             z-index: 1;
             animation-name: menuanim;
-            animation-duration: 2s;
+            animation-duration: 1s;
         }
         @-webkit-keyframes menuanim {
             from {width:1000px; height:0;}
-            to {width:1000px; height:300px;}
+            to {width:1000px; height:100px;}
         }

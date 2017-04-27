7aaff263eb6e94b606c76f91da6e2fb21ccb9bcd
bekkjorden26 <bektri16@student.westerdals.no>
2017-04-27 12:42:13 +0200

    Add files via upload

---
+++ steder.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<title>Navn</title>
+
+<body>
+     
+    <div class="w3-row">
+        <div class="w3-content w3-">
+            
+            <div class="w3-bar">
+            
+                <select class="w3-right">
+        
+                    <option>???</option>
+                    <option>Sorter etter</option>
+                    <option>text her</option>
+                    <option>text her</option>
+
+                </select>
+                
+            </div>
+
+            <!-- INNHOLD LAGES HER! -->  
+            <div style="height:600px">
+                
+                <div class="w3-left w3-twothird" style="height:100%"> 
+            
+                    <img src="1491419678_map-icon.png" alt="bilde" style="width:75%; margin:auto;">
+                     
+                    <p style="padding-left:10%;">det skal være text her det skal være text her<br>det skal være text her det skal være text her <br>det skal være text her det skal være text her <br> det skal være text her</p>
+            
+                </div>
+            
+            
+                <div class="w3-left w3-third" style="height:100%">
+            
+                   <div class="w3-full w3-center">
+                       
+                       <img  src="1491419678_map-icon.png" alt="kart" style="width:100%;">
+                    
+                    </div> 
+                    
+                    <div class="w3-border w3-black" style=" height:36%;"></div>
+    
+                </div>
+
+            </div>
+        </div>
+    </div>
+     
+     
+
+</body>
+</html>

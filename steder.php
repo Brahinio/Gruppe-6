179332cd6bf68d3b8a25ea035ddcd57bfe377e6e
Mathias Tellefsen <mathias-ds@hotmail.com>
2017-04-27 18:02:45 +0200

    La til header og footer i steder.php

--- steder.php
+++ steder.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
-
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="author" content="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<title>Navn</title>
-
+    <title>Navn</title>
+</head>
+    
+<?php 
+    require_once 'header.php';
+?>
+    
 <body>
      
     <div class="w3-row">
@@ -55,7 +60,9 @@
         </div>
     </div>
      
-     
+    <?php 
+        require_once 'footer.php';
+    ?>
 
 </body>
 </html>

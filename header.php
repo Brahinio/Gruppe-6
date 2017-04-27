22de69cb094b3756e5d153bd2fb045a5a661defe
Mathias Tellefsen <mathias-ds@hotmail.com>
2017-04-27 17:56:11 +0200

    Fikset meny linkene #2

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
             <div class="w3-row g6-zindex-top">
                 <div class="w3-threequarter">
                     <div class="w3-half">
-                        <a href="./index.php" alt="forside"><h3 id="logo">OVERSKRIFT</h3></a>
+                        <a href="./" alt="forside"><h3 id="logo">OVERSKRIFT</h3></a>
                     </div>
 
                     <!-- Icon menu -->
@@ -68,7 +68,8 @@
                             </span>
                         </button>
                         <div id="menuMain" class="g6-menu g6-bg g6-shadow-2 w3-dropdown-content w3-bar-block">
-                            <a href="./index.php" class="g6-menu-style w3-bar-item w3-button">Hjem</a>
+                            <a href="./" class="g6-menu-style w3-bar-item w3-button">Hjem</a>
+                            <a href="./steder.php" class="g6-menu-style w3-bar-item w3-button">Steder</a>
                             <a href="./kart.php" class="g6-menu-style w3-bar-item w3-button">Kart</a>
                             <a href="./forslag.php" class="g6-menu-style w3-bar-item w3-button">Forslag</a>
                         </div>

c407f7eae40470ec179935efc5e5d2cc81517dfa
Morten Wilsgård <Morten Wilsgrd>
2017-05-13 16:34:32 +0200

    endret farge på introoverskrift og lagt til ingress

--- Kart.php
+++ Kart.php
@@ -21,7 +21,10 @@ $locations = Location::all();
 <body>
     <div class="w3-row">
         <div class="w3-content g6-padding">
-            <div class="g6-center"><h1>Kart</h1></div>
+            
+            <div class="g6-center"><h1 class="g6-color-blue">Kart</h1>
+                <p class="ingress">Her får du en kartoversikt over anbefalte steder!</p>
+            </div>
 
             <div class="w3-quarter g6-bg">
                 <form class="sok-kategorier">
--- steder.php
+++ steder.php
@@ -56,7 +56,10 @@ $categories = Category::where('id', $matbutikkerId)->orWhere('id', $restauranter
      
     <div class="w3-row">
         <div class="w3-content g6-padding">
-            <div class="g6-center"><h1>Steder</h1></div>
+            
+            <div class="g6-center"><h1 class="g6-color-blue">Steder</h1>
+                <p class="ingress">Her kan du sjekke ut alle anbefalinger!</p>
+            </div>
             
             <div class="w3-bar">
             
